257209ec64521636c2d2dc59ac4c78433ec94bde
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-15 00:01:02 +0530

    Fix deprecated parameter in backend.tf and resolved provider validation issues

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "gaurav-app-storage-bucket"
-    key            = "terraform/infra.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    encrypt        = true
+    lock_table     = "terraform-locks"  # Correct param name
   }
 }
