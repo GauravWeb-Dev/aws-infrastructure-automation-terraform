c998ee37391513156e1876a65642ae47d0f2e9de
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-15 00:48:51 +0530

    Initial commit: Terraform project to provision VPC, EC2, and S3 with remote backend

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 (Mumbai)
+  ami                    = "ami-0e35ddab05955cf57"  # Amazon Linux 2 (Mumbai)
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   associate_public_ip_address = true
